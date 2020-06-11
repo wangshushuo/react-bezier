7a130ad3d74d50cc70c9f15ff5886cbe2a660433
王书硕 <wangshushuo@edinnovaedu.com>
2020-06-11 23:40:02 +0800

    update docs

---
+++ docs/build/precache-manifest.16d76597dd1ce3ebdbc879a76fb1d6e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfb386b660a00327d685e7f34681f4ba",
+    "url": "/react-bezier/index.html"
+  },
+  {
+    "revision": "f76eaed7e48e875004cc",
+    "url": "/react-bezier/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5ef769a91baa98ee72f9",
+    "url": "/react-bezier/static/js/2.3343174c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-bezier/static/js/2.3343174c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f76eaed7e48e875004cc",
+    "url": "/react-bezier/static/js/main.6f9120b2.chunk.js"
+  },
+  {
+    "revision": "18dd2719948f01cd5931",
+    "url": "/react-bezier/static/js/runtime-main.b6587243.js"
+  }
+]);
